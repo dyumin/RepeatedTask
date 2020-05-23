bca1a1ebc0a3ee291fa0bfddda0cb03cbb9314fc
Dyumin <jejni.j@gmail.com>
2020-05-23 19:41:38 +0300

    Added move semantics

--- include/RepeatedTask/RepeatedTask.h
+++ include/RepeatedTask/RepeatedTask.h
@@ -6,12 +6,15 @@
 #include <chrono>
 #include <condition_variable>
 
-template<class Fp, class Rep, class Period>
+template<class Fp, class Rep = std::chrono::nanoseconds::rep, class Period = std::chrono::nanoseconds::period>
 class RepeatedTask
 {
 public:
     typedef Fp callback_type;
     typedef std::chrono::duration<Rep, Period> period_type;
+
+    template<class, class, class>  // all template instantiations are friends to each other
+    friend class RepeatedTask;
 
     RepeatedTask() noexcept = default;
 
@@ -20,6 +23,59 @@ public:
         , m_period(period)
     {
         std::unique_lock<decltype(m_mutex)> lock(m_mutex);
+        start(lock);
+    }
+
+    ~RepeatedTask() noexcept
+    {
+        std::unique_lock<decltype(m_mutex)> lock(m_mutex);
+        if (!m_stopped)
+            stop(lock);
+    }
+
+    RepeatedTask(RepeatedTask&& other) noexcept
+    {
+        std::unique_lock<decltype(m_mutex)> otherLock(other.m_mutex);
+        const bool otherStopped = other.m_stopped;
+        if (!otherStopped)
+            other.stop(otherLock);
+
+        m_f = std::move(other.m_f);
+        m_period = other.m_period;
+
+        if (!otherStopped)
+        {
+            std::unique_lock<decltype(m_mutex)> lock(m_mutex);
+            start(lock);
+        }
+    }
+
+    template<class Fp1, class Rep1, class Period1>
+    RepeatedTask& operator=(RepeatedTask<Fp1, Rep1, Period1>&& other) noexcept
+    {
+        std::unique_lock<decltype(m_mutex)> otherLock(other.m_mutex);
+        const bool otherStopped = other.m_stopped;
+        if (!otherStopped)
+            other.stop(otherLock);
+
+        std::unique_lock<decltype(m_mutex)> lock(m_mutex);
+
+        m_f = std::move(other.m_f);
+        m_period = other.m_period;
+
+        const bool& stopped = m_stopped;
+        if (stopped && !otherStopped)
+            start(lock);
+        else if (!stopped && otherStopped)
+            stop(lock);
+
+        return *this;
+    }
+
+private:
+    template <class Lock>
+    void start(Lock& lock) noexcept
+    {
         m_thread = std::thread(&RepeatedTask::run, this);
         m_cv.wait(lock, [&]()
         {
@@ -27,27 +83,20 @@ public:
         });
     }
 
-    ~RepeatedTask() noexcept
+    template <class Lock>
+    void stop(Lock& lock) noexcept
     {
-        std::unique_lock<decltype(m_mutex)> lock(m_mutex);
-        if (!m_stopped)
+        m_shouldStop = true;
+        m_cv.notify_one();
+        m_cv.wait(lock, [&]()
         {
-            m_shouldStop = true;
-            m_cv.wait(lock, [&]()
-            {
-                return m_stopped;
-            });
-        }
+            return m_stopped;
+        });
 
         if (m_thread.joinable())
             m_thread.join();
     }
 
-    // noncopyable
-    RepeatedTask(const RepeatedTask&) = delete;
-    RepeatedTask& operator=(const RepeatedTask&) = delete;
-
-private:
     void run()
     {
         std::unique_lock<decltype(m_mutex)> lock(m_mutex);
@@ -70,7 +119,7 @@ private:
     }
 
 private:
-    const callback_type m_f;
+    callback_type m_f;
     period_type m_period;
 
     std::condition_variable m_cv;
